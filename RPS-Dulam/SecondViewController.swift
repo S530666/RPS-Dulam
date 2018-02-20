a1ad39859bb2ce64a47edd52543a85a5913b3ed4
student <s530666@ch112010m.nwmissouri.edu>
2018-02-20 14:00:31 -0600

    Initial Commit

---
+++ RPS-Dulam/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RPS-Dulam
+//
+//  Created by Dulam,Surya Chandra Phani Santhosh on 2/20/18.
+//  Copyright © 2018 Dulam,Surya Chandra Phani Santhosh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
